8fa4f242fa8b39225e46da4bff80844595e77a58
Davide Ficano <davide.ficano@gmail.com>
2020-10-18 07:31:35 +0200

    Updates

---
+++ precache-manifest.ef0859e38cb1aa26452e5c8eb566ee3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582fff783eb87ac89c8de78f1176a46e",
+    "url": "/mylinks/index.html"
+  },
+  {
+    "revision": "ddfe9a2d11e9eb16ad41",
+    "url": "/mylinks/static/css/main.0d92d296.chunk.css"
+  },
+  {
+    "revision": "0a873fe3cfa8d68a5b99",
+    "url": "/mylinks/static/js/2.9ed48a11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mylinks/static/js/2.9ed48a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfe9a2d11e9eb16ad41",
+    "url": "/mylinks/static/js/main.9928d393.chunk.js"
+  },
+  {
+    "revision": "e31a2845c57b21d2480c",
+    "url": "/mylinks/static/js/runtime-main.97cf2ac4.js"
+  }
+]);
