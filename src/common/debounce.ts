f668129314a8bf1e9a902e7321d548d4cfacea49
Davide Ficano <davide.ficano@gmail.com>
2023-06-24 10:16:31 +0200

    return stop function, too

--- src/common/debounce.ts
+++ src/common/debounce.ts
@@ -1,13 +1,26 @@
 // https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086
-export const debounce = (func: (...args: unknown[]) => void, wait: number): (...args: unknown[]) => void => {
+/**
+ * Trigger the func only after the wait timeout
+ *
+ * @param func the callback called at expire time
+ * @param wait the time to wait in millisecond
+ * @returns array containing two functions
+ * the first must be called from event listener
+ * the second could be called to stop programmatically the waiting bound
+ */
+export const debounce = (func: (...args: unknown[]) => void, wait: number): [(...args: unknown[]) => void, () => void] => {
   let timeout: NodeJS.Timeout;
 
-  return function executedFunction(...args: unknown[]): void {
-    const later = (): void => {
-      func(...args);
-    };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
+  return [
+    (...args: unknown[]): void => {
+      const later = (): void => {
+        func(...args);
+      };
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    },
+    (): void => {
+      clearTimeout(timeout);
+    }
+  ];
 };
-
