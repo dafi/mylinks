eda8f6bf9fd94c0ec1ac5da251b6e45df4bb1eac
Davide Ficano <davide.ficano@gmail.com>
2023-07-16 09:55:19 +0200

    migrated App and Page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Config from '../../common/Config';
 import { isEditLinkData, prepareForSave } from '../../common/EditHelper';
 import { isKeyboardEventConsumer } from '../../common/HtmlUtil';
@@ -14,35 +14,17 @@ import { EditLinkDialog } from '../editLinkDialog/EditLinkDialog';
 import { LinkFinderDialog } from '../linkFinderDialog/LinkFinderDialog';
 import { Grid } from '../widgets/grid/Grid';
 import './App.css';
+import { getHideShortcuts, toggleHideShortcuts } from './App.utils';
 import { AppToolbar, AppToolbarActionType } from './AppToolbar';
 import './toolbar-icon.css';
 
-const STORAGE_PREF_HIDE_SHORTCUTS = 'hideShortcuts';
-
-// https://github.com/microsoft/TypeScript/issues/21309
-// TS doesn't include experimental APIs in lib.d.ts
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const window: any;
-
-interface PageState {
-  myLinks?: MMLinks;
-  uiState: AppUIState;
-  isFinderOpen: boolean;
-  isEditLinkOpen?: boolean;
-  editLinkData?: EditLinkData;
+interface LinkDialogData {
+  isOpen: boolean;
+  data?: EditLinkData;
 }
 
-class Page extends React.Component<unknown, PageState> {
-  constructor(props: unknown) {
-    super(props);
-    const uiState: AppUIState = {
-      hideShortcuts: this.hideShortcuts,
-      onEdit: (editLinkData): void => this.onEditData(editLinkData),
-    };
-    this.state = { isFinderOpen: false, uiState };
-  }
-
-  keyDown(e: KeyboardEvent): boolean {
+function Page(): JSX.Element {
+  function keyDown(e: KeyboardEvent): boolean {
     const isThisTarget = e.currentTarget === e.target || !isKeyboardEventConsumer(e.target as HTMLElement);
 
     // ignore events bubbling from other listeners
@@ -54,141 +36,137 @@ class Page extends React.Component<unknown, PageState> {
       e.stopPropagation();
       e.preventDefault();
 
-      this.showLinkFinder(true);
+      setIsFinderOpen(true);
       return true;
     }
     return UIInput.instance().keyDown(e);
   }
 
-  componentDidMount(): void {
-    document.body.addEventListener('keydown', (e) => this.keyDown(e), false);
-    Config.fromData((myLinks: MMLinks | undefined) => {
-      this.setState({ myLinks });
-    });
-  }
-
-  onLinkSelected = (link: Link): void => {
+  const onLinkSelected = (link: Link): void => {
     // Ensure the DOM is updated and the dialog is hidden when the link is open
     // This is necessary because when returning to myLinks window/tab, the dialog can be yet visible
     window.requestIdleCallback(() => openLink(link));
   };
 
-  showLinkFinder(isOpen: boolean): void {
-    this.setState({ isFinderOpen: isOpen });
-  }
-
-  showEditLinkDialog(isOpen: boolean, editLinkData?: EditLinkData): void {
-    if (editLinkData?.editType === 'delete') {
-      this.onSave(editLinkData);
+  function showEditLinkDialog(isOpen: boolean, data?: EditLinkData): void {
+    if (data?.editType === 'delete') {
+      onSave(data);
       return;
     }
-    this.setState({ isEditLinkOpen: isOpen, editLinkData });
+    setEditLinkDialog({ isOpen, data });
   }
 
-  private onClickToolbar(e: MyLinksEvent<AppToolbarActionType>): void {
+  function onClickToolbar(e: MyLinksEvent<AppToolbarActionType>): void {
     if (e.target === 'loadConfig') {
-      this.onLoadConfig(e.data as File);
+      onLoadConfig(e.data as File);
     } else if (e.target === 'shortcut') {
-      this.onShortcut();
+      onShortcut();
     } else if (e.target === 'saveConfig') {
-      this.onSaveConfig();
+      onSaveConfig();
     }
   }
 
-  private onSaveConfig(): void {
-    if (this.state.myLinks) {
+  function onSaveConfig(): void {
+    if (myLinks) {
       const indentSpaces = 2;
       const w = window.open();
-      w.document.write(`<pre>${JSON.stringify(this.state.myLinks, null, indentSpaces)}</prev>`);
+      w?.document.write(`<pre>${JSON.stringify(myLinks, null, indentSpaces)}</prev>`);
     }
   }
 
-  private onShortcut(): void {
-    this.hideShortcuts = !this.hideShortcuts;
-    this.setState({
-      uiState: {
-        hideShortcuts: this.hideShortcuts,
-      }
+  function onShortcut(): void {
+    toggleHideShortcuts();
+    setUiState({
+      hideShortcuts: getHideShortcuts(),
     });
   }
 
-  private onEditData(editData: EditDataType): void {
+  function onEditData(editData: EditDataType): void {
     if (isEditLinkData(editData)) {
       if (editData.editType === 'update' || editData.editType === 'create') {
-        this.showEditLinkDialog(true, editData);
+        showEditLinkDialog(true, editData);
       } else {
-        this.onSave(editData);
+        onSave(editData);
       }
     } else {
-      this.onSave(editData);
+      onSave(editData);
     }
   }
 
-  private onLoadConfig(file: File): void {
-    Config.fromFile(file, (myLinks: MMLinks | undefined) => {
-      this.setState({ myLinks });
+  function onLoadConfig(file: File): void {
+    Config.fromFile(file, (mmLinks: MMLinks | undefined) => {
+      setMyLinks(mmLinks);
     });
   }
 
-  private onSave(editLinkData: EditLinkData | EditWidgetData): void {
-    if (!this.state.myLinks) {
+  function onSave(data: EditLinkData | EditWidgetData): void {
+    if (!myLinks) {
       return;
     }
     try {
-      if (prepareForSave(editLinkData)) {
-        Config.saveData(this.state.myLinks, (myLinks) => this.setState({ myLinks }));
+      if (prepareForSave(data)) {
+        Config.saveData(myLinks, (mmLinks) => setMyLinks(mmLinks));
       }
     } catch (e) {
       alert((e as Error).message);
     }
   }
 
-  renderLinkFinder(): ReactNode {
-    if (this.state.isFinderOpen) {
-      return <LinkFinderDialog isOpen={this.state.isFinderOpen}
-                               onClose={(): void => this.showLinkFinder(false)}
-                               onLinkSelected={this.onLinkSelected}
-                               widgets={this.state.myLinks?.columns}/>;
+  function renderLinkFinder(): ReactNode {
+    if (isFinderOpen) {
+      return <LinkFinderDialog isOpen={isFinderOpen}
+                               onClose={(): void => setIsFinderOpen(false)}
+                               onLinkSelected={onLinkSelected}
+                               widgets={myLinks?.columns}/>;
     }
     return null;
   }
 
-  renderEditLinkDialog(): ReactNode {
-    if (this.state.isEditLinkOpen && this.state.editLinkData) {
-      return <EditLinkDialog isOpen={this.state.isEditLinkOpen}
-                             onSave={(o): void => this.onSave(o)}
-                             onClose={(): void => this.showEditLinkDialog(false)}
-                             data={this.state.editLinkData}/>;
+  function renderEditLinkDialog(): ReactNode {
+    const { isOpen, data } = editLinkDialog;
+    if (isOpen && data) {
+      return <EditLinkDialog isOpen={isOpen}
+                             onSave={onSave}
+                             onClose={(): void => showEditLinkDialog(false)}
+                             data={data}/>;
     }
     return null;
   }
 
-  render(): ReactNode {
-    return <AppConfigContextProvider myLinks={this.state.myLinks}>
-      <AppUIStateContext.Provider value={this.state.uiState}>
-        <div className="ml-wrapper">
-          <div className="ml-grid">
-            <Grid columns={this.state.myLinks?.columns || []}/>
-          </div>
+  const defaultUiState: AppUIState = {
+    hideShortcuts: getHideShortcuts(),
+    onEdit: onEditData,
+  };
 
-          <AppToolbar
-            action={(e): void => this.onClickToolbar(e)}/>
+  const [myLinks, setMyLinks] = useState<MMLinks>();
+  const [uiState, setUiState] = useState(defaultUiState);
+  const [isFinderOpen, setIsFinderOpen] = useState(false);
+  const [editLinkDialog, setEditLinkDialog] = useState<LinkDialogData>({ isOpen: false });
 
-          {this.renderLinkFinder()}
-          {this.renderEditLinkDialog()}
+  useEffect(() => {
+    document.body.addEventListener('keydown', keyDown, false);
+    Config.fromData((mmLinks: MMLinks | undefined) => {
+      setMyLinks(mmLinks);
+    });
+    return () => document.body.removeEventListener('keydown', keyDown);
+  }, []);
 
+  return <AppConfigContextProvider myLinks={myLinks}>
+    <AppUIStateContext.Provider value={uiState}>
+      <div className="ml-wrapper">
+        <div className="ml-grid">
+          <Grid columns={myLinks?.columns || []}/>
         </div>
-      </AppUIStateContext.Provider>
-    </AppConfigContextProvider>;
-  }
 
-  get hideShortcuts(): boolean {
-    return (localStorage.getItem(STORAGE_PREF_HIDE_SHORTCUTS) === '1') || false;
-  }
+        <AppToolbar
+          action={onClickToolbar}/>
 
-  set hideShortcuts(v: boolean) {
-    localStorage.setItem(STORAGE_PREF_HIDE_SHORTCUTS, v ? '1' : '0');
-  }
+        {renderLinkFinder()}
+        {renderEditLinkDialog()}
+
+      </div>
+    </AppUIStateContext.Provider>
+  </AppConfigContextProvider>;
 }
 
 function App(): JSX.Element {
