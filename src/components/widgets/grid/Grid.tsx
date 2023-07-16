7133524e12a909553c32045352de38c87978e7b5
Davide Ficano <davide.ficano@gmail.com>
2023-07-16 10:39:45 +0200

    migrated Grid

--- src/components/widgets/grid/Grid.tsx
+++ src/components/widgets/grid/Grid.tsx
@@ -1,16 +1,13 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Widget as MLWidget } from '../../../model/MyLinks-interface';
 import { Column } from '../column/Column';
 
-export interface GridProps {
+interface GridProps {
   columns: MLWidget[][];
 }
 
-export class Grid extends React.Component<GridProps, unknown> {
-  render(): ReactNode {
-    const widgets = this.props.columns || [];
-    const columnsEl = widgets.map(
-      (columns: MLWidget[], index: number) => <Column key={index} value={columns}/>);
-    return <section className="ml-columns">{columnsEl}</section>;
-  }
+export function Grid(props: GridProps): JSX.Element {
+  const columnsEl = props.columns.map(
+    (widget: MLWidget[], index: number) => <Column key={index} value={widget}/>);
+  return <section className="ml-columns">{columnsEl}</section>;
 }
