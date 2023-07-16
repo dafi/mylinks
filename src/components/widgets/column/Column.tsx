1da16313b6d78313430df2ec1f89631e1b069fa7
Davide Ficano <davide.ficano@gmail.com>
2023-07-16 10:39:58 +0200

    migrated Column

--- src/components/widgets/column/Column.tsx
+++ src/components/widgets/column/Column.tsx
@@ -1,14 +1,12 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Widget as MLWidget } from '../../../model/MyLinks-interface';
 import { Widget } from '../widget/Widget';
 
-export interface ColumnProps {
+interface ColumnProps {
   value: MLWidget[];
 }
 
-export class Column extends React.Component<ColumnProps, unknown> {
-  render(): ReactNode {
-    const widgets = this.props.value.map(widget => <Widget key={widget.id} value={widget}/>);
-    return <section className="ml-rows">{widgets}</section>;
-  }
+export function Column(props: ColumnProps): JSX.Element {
+  const widgets = props.value.map(widget => <Widget key={widget.id} value={widget}/>);
+  return <section className="ml-rows">{widgets}</section>;
 }
