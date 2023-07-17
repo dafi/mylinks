055db46bde95e949e5ae4478810d838bdfbdabc5
Davide Ficano <davide.ficano@gmail.com>
2023-07-17 11:41:01 +0200

    migrated DraggableListItem

--- src/components/widgets/widget/DraggableListItem.tsx
+++ src/components/widgets/widget/DraggableListItem.tsx
@@ -12,28 +12,24 @@ export interface DraggableListItemProps {
   onDrop: (sourceId: string, destId: string) => void;
 }
 
-export class DraggableListItem extends React.Component<DraggableListItemProps, unknown> {
-  constructor(props: DraggableListItemProps) {
-    super(props);
-  }
-
-  handleDragStart(e: LIDragEvent): void {
+export function DraggableListItem(props: DraggableListItemProps): JSX.Element {
+  function handleDragStart(e: LIDragEvent): void {
     e.dataTransfer.setData('text/plain', e.currentTarget.id);
     e.currentTarget.classList.add('dragging');
     e.dataTransfer.effectAllowed = 'move';
   }
 
-  handleDragOver(e: LIDragEvent): void {
+  function handleDragOver(e: LIDragEvent): void {
     e.preventDefault();
     e.currentTarget.classList.add('over');
     e.dataTransfer.dropEffect = 'move';
   }
 
-  handleDragLeave(e: LIDragEvent): void {
+  function handleDragLeave(e: LIDragEvent): void {
     e.currentTarget.classList.remove('over');
   }
 
-  handleDrop(e: LIDragEvent): void {
+  function handleDrop(e: LIDragEvent): void {
     e.stopPropagation();
     const dragged = document.getElementById(e.dataTransfer.getData('text/plain'));
     if (dragged?.parentNode) {
@@ -42,30 +38,28 @@ export class DraggableListItem extends React.Component<DraggableListItemProps, u
         dragged.parentNode.removeChild(dragged);
         dropped?.insertAdjacentElement('beforebegin', dragged);
 
-        this.props.onDrop(dragged.id, dropped.id);
+        props.onDrop(dragged.id, dropped.id);
       }
     }
     e.currentTarget.classList.remove('over');
   }
 
-  handleDragEnd(e: LIDragEvent): void {
+  function handleDragEnd(e: LIDragEvent): void {
     e.currentTarget.classList.remove('over', 'dragging');
   }
 
-  render(): ReactNode {
-    const dragProps = this.props.draggable ? {
-      draggable: true,
-      onDragStart: (e: LIDragEvent): void => this.handleDragStart(e),
-      // onDragEnter: (e: LIDragEvent): void => this.handleDragEnter(e),
-      onDragOver: (e: LIDragEvent): void => this.handleDragOver(e),
-      onDragLeave: (e: LIDragEvent): void => this.handleDragLeave(e),
-      onDrop: (e: LIDragEvent): void => this.handleDrop(e),
-      onDragEnd: (e: LIDragEvent): void => this.handleDragEnd(e),
-    } : null;
-    return (
-      <li id={this.props.id} {...dragProps}>
-        {this.props.children}
-      </li>
-    );
-  }
+  const dragProps = props.draggable ? {
+    draggable: true,
+    onDragStart: handleDragStart,
+    // onDragEnter: handleDragEnter,
+    onDragOver: handleDragOver,
+    onDragLeave: handleDragLeave,
+    onDrop: handleDrop,
+    onDragEnd: handleDragEnd,
+  } : null;
+  return (
+    <li id={props.id} {...dragProps}>
+      {props.children}
+    </li>
+  );
 }
