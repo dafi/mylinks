6cb345251a6e72a1e53bbea62f06e22780f6105b
Davide Ficano <davide.ficano@gmail.com>
2023-07-17 11:53:20 +0200

    migrated WidgetToolbar

--- src/components/widgets/widgetToolbar/WidgetToolbar.tsx
+++ src/components/widgets/widgetToolbar/WidgetToolbar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { MyLinkActionCallback } from '../../../model/Events';
 import { openAllLinks } from '../../../model/MyLinks';
 import { Widget as MLWidget } from '../../../model/MyLinks-interface';
@@ -14,29 +14,23 @@ interface WidgetToolbarProps {
   editable: boolean;
 }
 
-export class WidgetToolbar extends React.Component<WidgetToolbarProps, unknown> {
-  constructor(props: WidgetToolbarProps) {
-    super(props);
-  }
+export function WidgetToolbar(props: WidgetToolbarProps): JSX.Element {
+  const collapseIcon = props.collapsed ? 'fa fa-angle-down' : 'fa fa-angle-up';
+  const collapseTitle = props.collapsed ? 'Expand content' : 'Collapse content';
+  const classNames = props.classNames ?? '';
+  const editStyle: React.CSSProperties = {
+    color: props.editable ? 'var(--action-color-primary)' : ''
+  };
+  const editClassNames = props.editable ? 'fas fa-toggle-on' : 'fas fa-toggle-off';
 
-  render(): ReactNode {
-    const collapseIcon = this.props.collapsed ? 'fa fa-angle-down' : 'fa fa-angle-up';
-    const collapseTitle = this.props.collapsed ? 'Expand content' : 'Collapse content';
-    const classNames = this.props.classNames ?? '';
-    const editStyle: React.CSSProperties = {
-      color: this.props.editable ? 'var(--action-color-primary)' : ''
-    };
-    const editClassNames = this.props.editable ? 'fas fa-toggle-on' : 'fas fa-toggle-off';
-
-    return (
-      <span className={`ml-toolbar ${classNames}`}>
+  return (
+    <span className={`ml-toolbar ${classNames}`}>
         <i className={`${editClassNames} icon`}
            style={editStyle}
-           onClick={(): void => this.props.action({ target: 'edit' })}
+           onClick={(): void => props.action({ target: 'edit' })}
            title="Toggle Edit Mode"/>
-        <i className="fa fa-external-link-alt icon" onClick={(): void => openAllLinks(this.props.widget)} title="Open all links"/>
-        <i className={`${collapseIcon} icon`} onClick={(): void => this.props.action({ target: 'collapse' })} title={collapseTitle}/>
+        <i className="fa fa-external-link-alt icon" onClick={(): void => openAllLinks(props.widget)} title="Open all links"/>
+        <i className={`${collapseIcon} icon`} onClick={(): void => props.action({ target: 'collapse' })} title={collapseTitle}/>
       </span>
-    );
-  }
+  );
 }
