364595bc84e25ec4fb242f60e3f164498102c9d4
Davide Ficano <davide.ficano@gmail.com>
2023-07-18 08:52:36 +0200

    migrated EditLinkDialog

--- src/components/editLinkDialog/EditLinkDialog.tsx
+++ src/components/editLinkDialog/EditLinkDialog.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, ReactNode, RefObject } from 'react';
-import { EditLinkData, LinkEditedProperties } from '../../model/EditData-interface';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { EditLinkData } from '../../model/EditData-interface';
 import { DialogProps } from '../modal/Dialog';
 import Modal from '../modal/Modal';
 import './EditLinkDialog.css';
@@ -9,111 +9,104 @@ export interface EditLinkDialogProps extends DialogProps {
   onSave: (editLinkData: EditLinkData) => void;
 }
 
-// https://stackoverflow.com/questions/57773734/how-to-use-partially-the-computed-property-name-on-a-type-definition/57774343#57774343
-// compound properties must be strings so, we allow to index elements by string
-type EditLinkDialogState = LinkEditedProperties & Record<string, string | undefined>;
-
-export class EditLinkDialog extends React.Component<EditLinkDialogProps, EditLinkDialogState> {
-  private inputRef: RefObject<HTMLInputElement> = React.createRef();
-
-  constructor(props: EditLinkDialogProps) {
-    super(props);
-    const { label, url, shortcut, favicon } = props.data.link;
-    this.state = { label, url, shortcut, favicon };
+export function EditLinkDialog({ isOpen, data, onSave, onClose }: EditLinkDialogProps): JSX.Element {
+  function onCloseDialog(): void {
+    onClose();
   }
 
-  onClose(): void {
-    this.props.onClose();
-  }
-
-  moveFocusToSearch(): void {
-    const el = this.inputRef?.current;
-    if (el) {
-      el.focus();
-    }
-  }
-
-  componentDidMount(): void {
-    this.moveFocusToSearch();
-  }
-
-  private onClickSave(e: React.MouseEvent<HTMLButtonElement>): void {
+  function onClickSave(e: React.MouseEvent<HTMLButtonElement>): void {
     e.preventDefault();
 
-    this.props.onSave({ ...this.props.data, editedProperties: this.state });
-    this.onClose();
+    onSave({ ...data, editedProperties: { label, url, shortcut, favicon } });
+    onCloseDialog();
   }
 
-  onChange(e: ChangeEvent<HTMLInputElement>): void {
+  function onChange(e: ChangeEvent<HTMLInputElement>): void {
     const action = e.target.dataset.action;
     if (action) {
-      this.setState({ [action]: e.target.value });
+      actionMap[action](e.target.value);
     }
   }
 
-  render(): ReactNode {
-    const { label, url, shortcut, favicon } = this.state;
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
-    return (
-      <Modal isOpen={this.props.isOpen}
-             onClose={(): void => this.onClose()}>
-        <div className="edit-link-dialog">
-          <h2 className="title">Edit Link</h2>
+  const inputRef = React.createRef<HTMLInputElement>();
 
-          <form>
-            <ul className="flex-outer">
-              <li>
-                <label htmlFor="link-label">Label</label>
-                <input
-                  data-action='label'
-                  ref={this.inputRef}
-                  type="text"
-                  defaultValue={label}
-                  onChange={(e): void => this.onChange(e)}
-                  placeholder="Videos"
-                />
-              </li>
-              <li>
-                <label htmlFor="link-url">Url</label>
-                <input
-                  data-action="url"
-                  type="text"
-                  defaultValue={url}
-                  onChange={(e): void => this.onChange(e)}
-                  placeholder="https://youtube.com"/>
-              </li>
-              <li>
-                <label htmlFor="shortcut">Favicon URL</label>
-                <input
-                  data-action="favicon"
-                  type="text"
-                  defaultValue={favicon}
-                  onChange={(e): void => this.onChange(e)}
-                  placeholder="favicon url"/>
-              </li>
-              <li>
-                <label htmlFor="shortcut">Shortcut</label>
-                <input
-                  data-action="shortcut"
-                  type="text"
-                  defaultValue={shortcut}
-                  onChange={(e): void => this.onChange(e)}
-                  placeholder="press the key combination to assign"/>
-              </li>
-              <li>
-                <div className="toolbar">
-                  <button
-                    className="text-white bg-action-primary hover"
-                    onClick={(e): void => this.onClickSave(e)}>Save</button>
-                  <button
-                    className="text-white bg-action-secondary hover"
-                    onClick={(): void => this.onClose()}>Cancel</button>
-                </div>
-              </li>
-            </ul>
-          </form>
-        </div>
-      </Modal>
-    );
-  }
+  const [label, setLabel] = useState(data.link.label);
+  const [url, setUrl] = useState(data.link.url);
+  const [shortcut, setShortcut] = useState(data.link.shortcut);
+  const [favicon, setFavicon] = useState(data.link.favicon);
+
+  const actionMap: Record<string, (v: string) => void> = {
+    label: setLabel,
+    url: setUrl,
+    shortcut: setShortcut,
+    favicon: setFavicon
+  };
+
+  return (
+    <Modal isOpen={isOpen}
+           onClose={onCloseDialog}>
+      <div className="edit-link-dialog">
+        <h2 className="title">Edit Link</h2>
+
+        <form>
+          <ul className="flex-outer">
+            <li>
+              <label htmlFor="link-label">Label</label>
+              <input
+                data-action="label"
+                ref={inputRef}
+                type="text"
+                defaultValue={label}
+                onChange={onChange}
+                placeholder="Videos"
+              />
+            </li>
+            <li>
+              <label htmlFor="link-url">Url</label>
+              <input
+                data-action="url"
+                type="text"
+                defaultValue={url}
+                onChange={onChange}
+                placeholder="https://youtube.com"/>
+            </li>
+            <li>
+              <label htmlFor="shortcut">Favicon URL</label>
+              <input
+                data-action="favicon"
+                type="text"
+                defaultValue={favicon}
+                onChange={onChange}
+                placeholder="favicon url"/>
+            </li>
+            <li>
+              <label htmlFor="shortcut">Shortcut</label>
+              <input
+                data-action="shortcut"
+                type="text"
+                defaultValue={shortcut}
+                onChange={onChange}
+                placeholder="press the key combination to assign"/>
+            </li>
+            <li>
+              <div className="toolbar">
+                <button
+                  className="text-white bg-action-primary hover"
+                  onClick={onClickSave}>Save
+                </button>
+                <button
+                  className="text-white bg-action-secondary hover"
+                  onClick={onCloseDialog}>Cancel
+                </button>
+              </div>
+            </li>
+          </ul>
+        </form>
+      </div>
+    </Modal>
+  );
 }
