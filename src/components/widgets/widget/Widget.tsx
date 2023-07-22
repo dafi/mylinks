0dc1cfaac112e15695a421f7e708fb1f0638ef51
Davide Ficano <davide.ficano@gmail.com>
2023-07-22 07:24:28 +0200

    fixed #20 Adding links with same url generate strange behaviour (dnd, edit)

--- src/components/widgets/widget/Widget.tsx
+++ src/components/widgets/widget/Widget.tsx
@@ -30,10 +30,10 @@ export function Widget({ value: widget }: WidgetProps): JSX.Element {
 
   function onDrop(sourceId: string, destId: string): void {
     const links = widget.list;
-    const fromIndex = links.findIndex(l => l.url === sourceId);
-    const toIndex = links.findIndex(l => l.url === destId);
-    if (context.onEdit && fromIndex >= 0 && toIndex >= 0) {
-      context.onEdit({
+    const fromIndex = links.findIndex(l => l.id === sourceId);
+    const toIndex = links.findIndex(l => l.id === destId);
+    if (appOnEdit && fromIndex >= 0 && toIndex >= 0) {
+      appOnEdit({
         link: links[fromIndex],
         editType: 'move',
         widget,
@@ -42,46 +42,47 @@ export function Widget({ value: widget }: WidgetProps): JSX.Element {
     }
   }
 
-  const context = useContext(AppUIStateContext);
+  const { onEdit: appOnEdit } = useContext(AppUIStateContext);
 
   const [editable, setEditable] = useState(false);
   const { startCollapsed, collapsed, setCollapsed, toggleStartCollapsed } = useCollapsed(widget.id);
 
   const items = widget.list.map(v =>
     <DraggableListItem
-      key={v.url}
-      id={v.url}
+      key={v.id}
+      id={v.id}
       draggable={editable}
       onDrop={(s, d): void => onDrop(s, d)}>
       <Link
         link={v}
         draggable={!editable}
         widget={widget}
-        editable={editable}/>
+        editable={editable} />
     </DraggableListItem>);
 
   const cls = cssExtraClasses(startCollapsed, collapsed);
 
   return (
-    <div className={`ml-widget ${cls.widget}`}
-         data-list-id={widget.id}
-         onMouseEnter={(): void => setCollapsed(false)}
-         onMouseLeave={(): void => setCollapsed(true)}>
+    <div
+      className={`ml-widget ${cls.widget}`}
+      data-list-id={widget.id}
+      onMouseEnter={(): void => setCollapsed(false)}
+      onMouseLeave={(): void => setCollapsed(true)}>
       <div>
         <h2 className="ml-widget-title">
-          <WidgetTitle editable={editable} widget={widget} onToggleEdit={onToggleEdit}/>
+          <WidgetTitle editable={editable} widget={widget} onToggleEdit={onToggleEdit} />
         </h2>
         <WidgetToolbar
           collapsed={startCollapsed}
           widget={widget}
           editable={editable}
           action={(e): void => onClickToolbar(e)}
-          classNames={'hover-toolbar'}/>
+          classNames="hover-toolbar" />
       </div>
       <div className="ml-widget-container">
         <div className="ml-widget-control-box">
           <ul className="ml-widget-list">{items}</ul>
-          <WidgetActionList editable={editable} widget={widget}/>
+          <WidgetActionList editable={editable} widget={widget} />
         </div>
       </div>
     </div>);
