f66d33368ce8c0291ac52e5adf2a6c596e4d00f7
Davide Ficano <davide.ficano@gmail.com>
2023-12-12 07:51:35 +0100

    created Reminder component

---
+++ src/components/reminder/Reminder.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from 'react';
+import './Reminder.css';
+
+type ReminderProps = {
+  readonly message: string;
+  readonly isVisible: boolean;
+  readonly onExportConfig: () => void;
+};
+
+export function ReminderComponent(
+  {
+    message,
+    isVisible,
+    onExportConfig,
+  }: ReminderProps
+): ReactElement | null {
+  if (!isVisible) {
+    return null;
+  }
+  return (
+    <div className="reminder">
+        <span className="message">{message}</span>
+        <button className="button" onClick={onExportConfig}>Save JSON</button>
+    </div>
+  );
+}
