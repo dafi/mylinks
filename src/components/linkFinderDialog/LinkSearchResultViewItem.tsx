48879b6fd55d914394728e5e978675992d3cd7d9
Davide Ficano <davide.ficano@gmail.com>
2024-03-25 09:12:52 +0100

    Show shortcut on search items

--- src/components/linkFinderDialog/LinkSearchResultViewItem.tsx
+++ src/components/linkFinderDialog/LinkSearchResultViewItem.tsx
@@ -4,8 +4,10 @@ import { isNotEmptyString } from '../../common/StringUtil';
 import { useAppConfigContext } from '../../contexts/AppConfigContext';
 import { Link } from '../../model/MyLinks-interface';
 import { MyLinksLookup } from '../../model/MyLinksLookup';
+import { ShortcutDetails } from '../shortcut/shortcutDetails/ShortcutDetails';
 import { LinkIcon } from '../widgets/linkIcon/LinkIcon';
 import { Highlight } from './Highlight';
+import './LinkSelector.css';
 
 function widgetTitle(link: Link, myLinksLookup: MyLinksLookup | undefined): string {
   const title = myLinksLookup?.linkManager.findWidgetByLinkId(link.id)?.title;
@@ -20,14 +22,16 @@ export function LinkSearchResultViewItem(
   const { myLinksLookup } = useAppConfigContext();
 
   return (
-    <>
-      <i className="list-image">
-        <LinkIcon link={item.link} />
-      </i>
-      <div>
-        <Highlight matches={item.matches} />
-        {widgetTitle(item.link, myLinksLookup)}
-      </div>
-    </>
+    <div className="link-search-result">
+      <ShortcutDetails combination={item.link.hotKey}>
+        <i className="list-image">
+          <LinkIcon link={item.link} />
+        </i>
+        <div>
+          <Highlight matches={item.matches} />
+          {widgetTitle(item.link, myLinksLookup)}
+        </div>
+      </ShortcutDetails>
+    </div>
   );
 }
