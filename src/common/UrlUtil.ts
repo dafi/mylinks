b5aab790c348a8d1812be26c5b8f01c34a381924
Davide Ficano <davide.ficano@gmail.com>
2024-04-01 09:12:55 +0200

    fixed incorrect result when specifier is not the first string character

--- src/common/UrlUtil.ts
+++ src/common/UrlUtil.ts
@@ -1,8 +1,7 @@
-// eslint-disable-next-line max-lines-per-function
 export function formatUrl(stringOrUrl: string | URL, format: string): string {
   const url = typeof stringOrUrl === 'string' ? new URL(stringOrUrl) : stringOrUrl;
-  let result = '';
   const len = format.length;
+  let result = '';
 
   for (let i = 0; i < len; i++) {
     const ch = format.charAt(i);
@@ -10,55 +9,46 @@ export function formatUrl(stringOrUrl: string | URL, format: string): string {
     switch (ch) {
       case '$':
         if (i + 1 < len) {
-          ++i;
-          const ch1 = format.charAt(i);
-          switch (ch1) {
-            case '$':
-              result += '$';
-              break;
-            case 'a':
-              result += url.hash;
-              break;
-            case 'h':
-              result += url.host;
-              break;
-            case 'H':
-              result += url.hostname;
-              break;
-            case 'u':
-              result += url.href;
-              break;
-            case 'o':
-              result += url.origin;
-              break;
-            case 'w':
-              result += url.password;
-              break;
-            case 'p':
-              result += url.pathname;
-              break;
-            case 'r':
-              result += url.port;
-              break;
-            case 't':
-              result += url.protocol;
-              break;
-            case 's':
-              result += url.search;
-              break;
-            case 'n':
-              result += url.username;
-              break;
-            default:
-              throw new Error(`Invalid format specifier '${ch1}'`);
-          }
+          result += parseSpecifier(url, format.charAt(++i));
         } else {
           throw new Error('Format specifier at end of string');
         }
         break;
       default:
         result += ch;
+        break;
     }
   }
   return result;
+}
+
+function parseSpecifier(url: URL, specifier: string): string {
+  switch (specifier) {
+    case '$':
+      return '$';
+    case 'a':
+      return url.hash;
+    case 'h':
+      return url.host;
+    case 'H':
+      return url.hostname;
+    case 'u':
+      return url.href;
+    case 'o':
+      return url.origin;
+    case 'w':
+      return url.password;
+    case 'p':
+      return url.pathname;
+    case 'r':
+      return url.port;
+    case 't':
+      return url.protocol;
+    case 's':
+      return url.search;
+    case 'n':
+      return url.username;
+    default:
+      throw new Error(`Invalid format specifier '${specifier}'`);
+  }
 }
