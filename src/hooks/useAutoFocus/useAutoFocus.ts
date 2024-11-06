731a3bcfc5f27867eaab4c59467962d0b9c8bd6d
Davide Ficano <davide.ficano@gmail.com>
2024-11-06 06:21:39 +0100

    fixed bad code practices

--- src/hooks/useAutoFocus/useAutoFocus.ts
+++ src/hooks/useAutoFocus/useAutoFocus.ts
@@ -2,9 +2,9 @@ import { DependencyList, RefObject, useEffect, useRef } from 'react';
 
 export function useAutoFocus<T extends HTMLElement>(
   initValue: T | null,
-  deps?: DependencyList | undefined
+  deps?: DependencyList
 ): RefObject<T> {
-  const ref = useRef<T>(initValue);
+  const ref = useRef(initValue);
 
   // eslint-disable-next-line react-compiler/react-compiler
   // eslint-disable-next-line react-hooks/exhaustive-deps
