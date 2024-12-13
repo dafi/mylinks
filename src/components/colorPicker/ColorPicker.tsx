c56aeed1290bb5538fca2a6e660b55ca87a4524f
Davide Ficano <davide.ficano@gmail.com>
2024-12-13 12:22:46 +0100

    renamed types

--- src/components/colorPicker/ColorPicker.tsx
+++ src/components/colorPicker/ColorPicker.tsx
@@ -2,33 +2,37 @@ import { ChangeEvent, ReactElement } from 'react';
 import { isNotEmptyString } from '../../common/StringUtil';
 import './ColorPicker.css';
 
-export type ColorItem = {
+export type ColorPickerItem = {
   id: string;
   label?: string;
   defaultValue?: string;
 };
 
 type ColorPickerProps = Readonly<{
-  items: ColorItem[];
-  onChange: (item: ColorItem, newColor: string) => void;
+  items: ColorPickerItem[];
+  onChange: (item: ColorPickerItem, newColor: string) => void;
 }>;
 
-export function ColorPicker({ items, onChange }: ColorPickerProps): ReactElement {
-  function onChangeColor(e: ChangeEvent<HTMLInputElement>, index: number): void {
-    onChange(items[index], e.target.value);
+export function ColorPicker(
+  {
+    items,
+    onChange,
+  }: ColorPickerProps
+): ReactElement {
+  function onChangeColor(item: ColorPickerItem, e: ChangeEvent<HTMLInputElement>): void {
+    onChange(item, e.target.value);
   }
 
   return (
     <div className="color-picker">
-      {items.map(({ label, defaultValue }, index) => {
-        const title = isNotEmptyString(label) ? <h2 className="title">{label}</h2> : null;
-
+      {items.map(item => {
+        const { id, label, defaultValue } = item;
         return (
-          <div key={label} className="column">
-            {title}
+          <div key={id} className="column">
+            {isNotEmptyString(label) ? <h2 className="title">{label}</h2> : null}
             <input
               type="color"
-              onChange={e => onChangeColor(e, index)}
+              onChange={e => onChangeColor(item, e)}
               defaultValue={defaultValue}
             />
           </div>
